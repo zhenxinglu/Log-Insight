e930e8b6739e0591a5918ffa069acd73092aa306
Zhenxing Lu <krisslu@outlook.com>
2025-05-06 15:49:40 +0800

    build: add icons to PyInstaller build configuration
    This commit updates both build.sh and build.bat to include the icons directory in the PyInstaller build process using the --add-data flag. This ensures that the icons are properly packaged with the application, improving the user experience by including necessary resources in the final build.

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ del /Q dist\* 2>nul
 del /Q build\* 2>nul
 
 echo packing...
-pyinstaller --onefile --windowed --name LogInsight log_insight.py
+pyinstaller --onefile --windowed --name LogInsight  --add-data "icons;icons" log_insight.py
 
 echo packing done, installer located in dist folder.
 pause
--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+echo "clean up the build folder..."
+rm -rf dist/* build/*
+
+echo "packing..."
+pyinstaller --onefile --windowed --name LogInsight --add-data "icons:icons" log_insight.py
+
+echo "packing done, installer located in dist folder."
